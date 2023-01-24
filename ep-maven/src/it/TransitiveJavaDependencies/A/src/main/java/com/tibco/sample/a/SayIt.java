1c4e0ec6548b7d3d7c6ddfea9434632de46fdeb3
tbouwens <tbouwens@tibco.com>
2023-01-24 10:36:55 -0800

    Update to Cloud Software Group

--- ep-maven/src/it/TransitiveJavaDependencies/A/src/main/java/com/tibco/sample/a/SayIt.java
+++ ep-maven/src/it/TransitiveJavaDependencies/A/src/main/java/com/tibco/sample/a/SayIt.java
@@ -1,7 +1,7 @@
 package com.tibco.sample.a;
 
 /*
- * Copyright (C) 2021, TIBCO Software Inc.
+ * Copyright (c) 2018-2023 Cloud Software Group, Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
